ab7dd298eb4c8091555daf50b090187347c9a4e2
XeryusTC <armadillo@onenetbeyond.org>
2015-07-05 18:49:27 +0200

    fix type

--- functional_tests/fabfile.py
+++ functional_tests/fabfile.py
@@ -1,7 +1,7 @@
 from fabric.api import env, run
 
 def _get_base_folder(host):
-    return '~/sites' + host
+    return '~/sites/' + host
 
 def _get_manage_dot_py(host):
     return '{path}/virtualenv/bin/python {path}/source/manage.py'.format(
